83439d98c4cb497bcd52a6aa36787256e6a40ad1
Jitesh Vedhera <vedherajitesh@gmail.com>
2019-01-13 10:04:12 -0800

    Update SendFile.java

--- SendFileChatProjects-master/SendFile-Client/src/client/SendFile.java
+++ SendFileChatProjects-master/SendFile-Client/src/client/SendFile.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 package client;
 
@@ -16,7 +11,7 @@ import javax.swing.JOptionPane;
 
 /**
  *
- * @author Hunk501
+ * @author Jitesh
  */
 public class SendFile extends javax.swing.JFrame {
 
