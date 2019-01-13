008bc5058f9edaa2d1affdaf96c1573adcf9faa1
Jitesh Vedhera <vedherajitesh@gmail.com>
2019-01-13 10:04:40 -0800

    Update SendingFileThread.java

--- SendFileChatProjects-master/SendFile-Client/src/client/SendingFileThread.java
+++ SendFileChatProjects-master/SendFile-Client/src/client/SendingFileThread.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 
 package client;
 
@@ -19,7 +15,7 @@ import javax.swing.JOptionPane;
 
 /**
  *
- * @author Hunk501
+ * @author Jitesh
  */
 public class SendingFileThread implements Runnable {
     
